80406fd16baf102ecb9cab1b5211c48152b5481c
EmanOthman21 <eothman21@gmail.com>
2021-01-04 19:46:53 +0200

    Add: Logical operations to ALU

--- src/ALU/ALU.vhd
+++ src/ALU/ALU.vhd
@@ -22,7 +22,7 @@ ARCHITECTURE a_alu OF alu IS
   Signal FADD, FADC, FSUB, FSBC : std_logic_vector(n-1 DOWNTO 0);
   Signal FAND, FFOR, FXOR, FCMP : std_logic_vector(n-1 DOWNTO 0);
   Signal FINC, FDEC, FCLR, FINV : std_logic_vector(n-1 DOWNTO 0);
-  Signal FLSR, FROR, FASR : std_logic_vector(n-1 DOWNTO 0);
+  Signal FLSR, FROR, FASR, FINVA : std_logic_vector(n-1 DOWNTO 0);
 
   Signal CADD, CADC, CSUB, CSBC : std_logic;
   Signal CAND, CFOR, CXOR, CCMP : std_logic;
@@ -32,25 +32,39 @@ ARCHITECTURE a_alu OF alu IS
   BEGIN
 
   -- logic
+  FINV <= not B;
+  FINVA <= not A;
   ADDER: addernbit PORT MAP(A, B, '0', FADD, CADD);
   ADDERCARRAY: addernbit PORT MAP(A, B, CIN, FADC, CADC);
-  SUBBTRACTOR: addernbit PORT MAP(A, "not" B, '1', FSUB, CSUB);
-  SUBBCARRY: addernbit PORT MAP(A, "not" B, '0', FSBC, CSBC);
+  SUBBTRACTOR: addernbit PORT MAP(A, FINV, '1', FSUB, CSUB);
+  SUBBCARRY: addernbit PORT MAP(A, FINV, '0', FSBC, CSBC);
+  COMPARATOR: addernbit PORT MAP(B, FINVA, '1', FCMP, CCMP);
+  INCREMENTOR: addernbit PORT MAP(B, (others => '0'), '1', FINC, CINC);
+  DECREMENTOR: addernbit PORT MAP(B, (others => '1'), '0', FDEC, CDEC);
+  
 
   -- map outputs
-    F <= A when selector = '00001'
-    else FADD when selector = '00010'
-    else FADC when selector = '00011'
-    else FSUB when selector = '00100' or (selector = '00101' and cin = '0')
-    else FSBC when selector = '00101' and cin = '1'
-    else (A and B) when selector = '00110'
-    else (A or B) when selector = '00111'
-    else (A xor B) when selector = '01000'
+    F <= A when selector = "00001"
+    else FADD when selector = "00010"
+    else FADC when selector = "00011"
+    else FSUB when selector = "00100" or (selector = "00101" and cin = '0')
+    else FSBC when selector = "00101" and cin = '1'
+    else (A and B) when selector = "00110"
+    else (A or B) when selector = "00111"
+    else (A xor B) when selector = "01000"
+    else FCMP when selector = "01001"
+    else FINC when selector = "01010"
+    else FDEC when selector = "01011"
+    else (others => '0') when selector = "01100"
+    else FINV when selector = "01101";
 
-    cout <= cin when selector = '00001'
-    else CADD when selector = '00010'
-    else CADC when selector = '00011'
-    else CSUB when selector = '00100' or (selector = '00101' and cin = '0')
-    else CSBC when selector = '00101' and cin = '1'
-    else '0' when selector = '00110' or selector = '00111' or selector = '01000'
+    cout <= cin when selector = "00001"
+    else CADD when selector = "00010"
+    else CADC when selector = "00011"
+    else CSUB when selector = "00100" or (selector = "00101" and cin = '0')
+    else CSBC when selector = "00101" and cin = '1'
+    else '0' when selector = "00110" or selector = "00111" or selector = "01000" or selector = "01100" or selector = "01101"
+    else CCMP when selector = "01001"
+    else CINC when selector = "01010"
+    else CDEC when selector = "01011";
 END a_alu;
