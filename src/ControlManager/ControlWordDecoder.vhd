d696bddae25dc1c36dadde2e869bb6cac784f00b
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2021-01-18 01:07:52 +0200

    Add Integration For PLA-BRANCH-OPERATION

--- src/ControlManager/ControlWordDecoder.vhd
+++ src/ControlManager/ControlWordDecoder.vhd
@@ -10,16 +10,56 @@ n : integer := 26
 	PORT(
 		uPC : INOUT std_logic_vector(8 DOWNTO 0);
 		ControlWord : IN std_logic_vector(n-1 DOWNTO 0);
-		HLT : OUT std_logic;
-		RST : OUT std_logic);
+		clk : IN std_logic;
+		HLT : INOUT std_logic;
+		RST : INOUT std_logic);
 END ENTITY ControlWord_Entity;
 
 ARCHITECTURE ControlWordDecoder OF ControlWord_Entity IS
 
+component ram_Entity IS
+GENERIC (
+n : integer := 16
+);
+	PORT(
+		rst:IN std_logic;
+		clk : IN std_logic;
+		MAR : INOUT  std_logic_vector(10 DOWNTO 0);
+		MDR : INOUT  std_logic_vector(n-1 DOWNTO 0);
+		ReadWriteSignals:IN std_logic_vector(1 DOWNTO 0));
+END component;
+
+component PLA IS
+	PORT (
+	operationType: IN std_logic_vector(3 DOWNTO 0);
+	IR: IN std_logic_vector(15 DOWNTO 0);
+	F9: IN std_logic_vector(2 DOWNTO 0);
+	branchCheck: IN std_logic;
+	plaOut: OUT std_logic_vector(8 DOWNTO 0)
+        );
+END component;
+
+COMPONENT branchFlag IS
+	PORT (
+	isBranch: IN std_logic;
+	branchCode: IN std_logic_vector(2 DOWNTO 0);
+	flags: IN std_logic_vector(1 DOWNTO 0);
+	FlagOut:OUT std_logic
+        );
+END COMPONENT;
+
+COMPONENT OperationSignals IS
+  PORT (IR : IN std_logic_vector (15 downto 0);
+  CodeWord : IN std_logic_vector (25 downto 0);
+  OperationType : OUT std_logic_vector (3 downto 0);
+  ALUSelector: OUT std_logic_vector (4 downto 0));
+END COMPONENT;
+
+
 COMPONENT alu IS
 Generic ( n : Integer:=16);
-  	PORT (A,B : IN std_logic_vector (n-1 downto 0);
-  	selector : IN std_logic_vector (4 downto 0);
+  PORT (A,B : IN std_logic_vector (n-1 downto 0);
+  selector : IN std_logic_vector (4 downto 0);
 	cin: IN std_logic;
 	F: OUT std_logic_vector (n-1 downto 0);
 	cout : OUT std_logic);
@@ -36,28 +76,37 @@ END COMPONENT;
 	Signal X : std_logic_vector(15 DOWNTO 0);
 	Signal Y : std_logic_vector(15 DOWNTO 0);
 	Signal Z : std_logic_vector(15 DOWNTO 0);
-	Signal FlagRegister : std_logic_vector(15 DOWNTO 0);
-
-	-- 0 Bit For Branch , 1 Bit For 1 operand , 2 Bit for 2 operands ,3 Bit for 0 Operand , EX: 0001 means Branch Operation --
-	Signal OperationType : std_logic_vector(3 DOWNTO 0);
-
-	-- Processor Data Bus --
 	Signal DataBus : std_logic_vector(15 DOWNTO 0);
+	SIGNAL ENABLEIN:std_logic_vector(14 DOWNTO 0);		
+	SIGNAL ENABLEOUT:std_logic_vector(14 DOWNTO 0);
+	SIGNAL RAMCLK:std_logic:=not clk;
+	Signal FlagRegister : std_logic_vector(2 DOWNTO 0);
+	Signal OperationType : std_logic_vector(3 DOWNTO 0);
+	Signal ALUselector : std_logic_vector(4 DOWNTO 0);
+	Signal FlagOut:std_logic;
+	-- Processor Data Bus --
+	
 
 
 	BEGIN
 	-- Registers Decoders SHOULD BE CALLED HERE For In\OUT And Out the results to the Registers Signals , Should be Called For All Registers AND OUT TO THE DATABUS --
 	
+	
+	-- Operation Type Handler	
+	OperationSignalsComponent: OperationSignals port map (IR,ControlWord,OperationType,ALUselector);
+
+	-- Branch Flag Test Handler
+	branchFlagComponent: branchFlag port map (OperationType(0),IR(11 DOWNTO 9),FlagRegister(1 DOWNTO 0),FlagOut);
+
+	
 	-- ALU SHOULD TAKE LAST 3 BITS FROM uPC TO CHECK IF CHANGE FLAG REG CARRY OR NOT --
-	ALUComponent : alu GENERIC MAP (16) port map (X,Y,ControlWord(15 downto 11),FlagRegister(0),Z,FlagRegister(0));
+	--ALUComponent : alu GENERIC MAP (16) port map (X,Y,ControlWord(15 downto 11),FlagRegister(0),Z,FlagRegister(0));
 
-	-- 0 OP HANDLER ENTITY SHOULD BE CALLED HERE AND TAKES CW BITS (10 DWONTO 9) AND OUT TO HLT/RST --
+	
 
-	-- OPERATION TYPE HANDLER (0-1-2-BRANCH) SHOULD BE CALLED HERE AND OUT To The OperationType Vector  --
+	--NEEDS MODIFICATION
+	PLAComponent:PLA port map (OperationType,IR,ControlWord(3 downto 1),FlagOut,uPC);
 
-	-- PLA SHOULD BE CALLED HERE AND TAKES CW (3 DOWNTO 1) BITS -F9- AND OPERATION TYPE VECTOR ,IR VECTOR SIGNAL ,CW (0) PLA BIT TO CHECK IF CHANGE uPC OR NOT THEN OUT TO THE uPC --
-
-	-- SET/CLEAR CARRY AND Y REG SHOULD BE CALLED HERE AND TAKES CW (5 DOWNTO) AND OUT TO THE Y SIGNAL AND FLAG REG(0) --
-
-	-- RAM SHOULD BE CALLED HERE AND TAKES CW (8 DOWNTO 7) FOR READWRITE , CW(6) FOR WFMC , MAR(10 DOWNTO 0) ,MDR THEN OUT TO THE MAR,MDR -WFMC NEEDS TO HANDLED HERE- --
+	-- RAM SHOULD BE CALLED HERE AND TAKES CW (8 DOWNTO 7) FOR READWRITE , MAR(10 DOWNTO 0) ,MDR THEN OUT TO THE MAR,MDR  --
+	RAMComponent : ram_Entity GENERIC MAP (16) port map (RST,RAMCLK,MAR(10 DOWNTO 0),MDR,ControlWord(8 downto 7));
 END ControlWordDecoder;
